9d9e445e9a74fb634e677865c0a39b8212bc4b9c
Sam Thomas <sgpthomas@gmail.com>
2018-06-15 15:38:52 -0700

    added verbosity option to merge.py

--- model_code/merge.py
+++ model_code/merge.py
@@ -10,23 +10,26 @@ def make_parser():
 
     parser.add_argument('resultsdir', action="store")
     parser.add_argument('output_name', action="store")
+    parser.add_argument('--verbose', '-v', action="store_true")
 
     return parser
 
-def merge_all(resultsdir):
+def merge_all(resultsdir, verbose=False):
     files = glob.glob("{}/*.pkl".format(resultsdir))
     df = pd.DataFrame()
     for f in files:
         try:
             new = pd.read_pickle(f)
-            print("dataset: {}".format(new['dataset']))
+            if verbose:
+                print("{} -> {}".format(f, new['dataset'].values[0]))
             df = pd.concat([df, new])
         except EOFError:
-            print("Found empty file called {}!".format(f))
+            if verbose:
+                print("Found empty file called {}!".format(f))
     return df.sort_values('dataset').reset_index(drop=True)
 
 if __name__ == "__main__":
     parser = make_parser().parse_args()
 
-    combined = merge_all(parser.resultsdir)
+    combined = merge_all(parser.resultsdir, verbose=parser.verbose)
     combined.to_pickle(parser.output_name)

feaf16549b58ba35fbf7100308a867ed4778b55d
Sam Thomas <sgpthomas@gmail.com>
2018-06-15 15:52:57 -0700

    fixed small bug in scheduler.py

--- model_code/scheduler.py
+++ model_code/scheduler.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
     s.settimeout(1)
     print("Starting to listen...")
     s.listen(options.max_connections)
-    while len(todo.remaining()) > 0:
+    while len(todos.remaining()) > 0:
         try:
             to_remove = []
             for k in clients:
